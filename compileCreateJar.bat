6951b0b3e1dbda18047336d138bd6bb3d79eb800
Ricardo Montania <ricardo@linuxafundo.com.br>
2014-12-15 12:15:00 -0200

    Update para versão 10 [parte 2]
    Envio do restante dos arquivos

--- compileCreateJar.bat
+++ compileCreateJar.bat
@@ -7,17 +7,24 @@ IF NOT EXIST bin (
 
 CD src\
 
-javac -d ..\bin\ contador\*.java
+javac -d ..\bin\ -cp ..\lib\* contador\*.java
 
 CD ..\bin\
 
-jar -cfm Contador.jar MANIFEST.MF contador\*
+jar cmf MANIFEST.txt Contador.jar contador\*
 
 CD ..
 
+
+
 IF EXIST bin\Contador.jar (
+	IF EXIST Contador.jar (
+		DEL Contador.jar
+	)
 	MOVE bin\Contador.jar
 	ECHO "Done"
 ) ELSE (
 	ECHO "Error"
 )
+
+PAUSE
