e44c1db3b0f16f57bba350f92f44c5cdc53bc4b0
Ricardo Campos <ricardo@ricardocampos.blog>
2021-03-09 16:29:14 -0300

    Updated run script

--- run.bat
+++ run.bat
@@ -1,10 +1,3 @@
 @ECHO OFF
 
-SET "JAVA_PATH="
-SET "VERSION=2.0.0"
-
-IF "%JAVA_PATH%" == "" (
-    SET "JAVA_PATH=java"
-)
-
-%JAVA_PATH% -jar target\task-timer-app-"%VERSION%".jar
+start javaw -jar target\task-timer-app-2.0.0.jar
